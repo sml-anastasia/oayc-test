e372739afb9e5cfc0636b630e74b16390b38e44c
Vassili Kiritsenko <vassili.kiritsenko@gmail.com>
2023-03-13 17:42:34 +0200

    use nextjs Image instead of HTML img

--- pages/staking.tsx
+++ pages/staking.tsx
@@ -1,5 +1,4 @@
 import { NextPage } from "next";
-import { useAccount } from "wagmi";
 import Head from "next/head";
 import React from "react";
 import StakingRedButton from "../components/Button/StakingRedButton";
@@ -9,14 +8,14 @@ import TopBar from "../components/Mutant/TopBar";
 import { useDefaultConnect } from "../hooks/web3/useDefaultConnect";
 import useEagerConnect from "../hooks/web3/useEagerConnect";
 import useChangeNetwork from "../hooks/web3/useChangeNetwork";
-import MutationWindow from "../components/Mutant/MutationWindow";
-import { device } from "../styles/device";
+import Image from "next/image";
 
 const Container = styled.div`
   position: relative;
   background: #f0ebdf;
-  min-width: 100vw;
+  width: 100%;
   min-height: 100vh;
+  overflow: hidden;
   display: flex;
   flex-direction: column;
   color: transparent;
@@ -26,25 +25,25 @@ const ContentContainer = styled.div`
   display: flex;
   max-width: 1200px;
   width: 100%;
-  justify-content: center;
   align-items: start;
   flex-direction: column;
   flex-grow: 1;
   margin: 0 auto;
-  padding: 140px 0px 0px;
+  z-index: 1;
 `;
 
-const StyledHero = styled.div`
+const StyledStakingContainer = styled.div`
   display: flex;
   justify-content: flex-start;
   align-items: start;
   flex-direction: column;
-  font-family: "Rubik";
+  font-family: "Rubik", serif;
   font-style: italic;
   font-weight: 700;
   font-size: 160px;
   line-height: 90%;
   gap: 20px;
+  margin-top: 145px;
 `;
 
 const StyledHead = styled.div`
@@ -87,14 +86,20 @@ const StyledIcon = styled.div`
   gap: 10px;
 `;
 
-const StyledHeroFooter = styled.div`
-  margin-top: 160px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+const StyledOAYCText = styled.div`
+  position: absolute;
+  bottom: calc(0px - 6vw);
+  width: 100%;
+  aspect-ratio: 1492/249;
+  max-height: 249px;
+
+  @media screen and (min-width: 1492px) {
+    bottom: -89px;
+  }
+
 `;
 
-const StyledApe = styled.img`
+const StyledApe = styled.div`
   position: absolute;
   right: 0;
   top: 100px;
@@ -103,57 +108,63 @@ const StyledApe = styled.img`
 `;
 
 const Staking: NextPage = () => {
-  useEagerConnect();
-  useChangeNetwork();
-  const { isConnected } = useAccount();
-  const { connect } = useDefaultConnect();
-  return (
-    <>
-      <Head>
-        <title>STAKING</title>
-        <link rel="icon" href="/main/favicons/red.svg" />
-      </Head>
-      <Container>
-        <TopBar
-          logoUrl="/main/favicons/red.svg"
-          socials={true}
-          pageType="staking"
-        />
-        <ContentContainer>
-          <StyledHero>
-            <StyledHead>
-              <StyledText>Stake</StyledText>
-              <StyledText2>or lock</StyledText2>
-            </StyledHead>
-            <StyledButtons>
-              <StakingRedButton onClick={() => connect()}>
-                Connect Wallet
-              </StakingRedButton>
-              <StakingWhiteButton>
-                <StyledIcon>
-                  <StyledText4>Rules and conditions</StyledText4>
-                  <img
-                    src="/images/svg/read_conditions.svg"
-                    alt="Rules and Conditions"
-                    width="41px"
-                    height="41px"
-                  />
-                </StyledIcon>
-              </StakingWhiteButton>
-            </StyledButtons>
-          </StyledHero>
-        </ContentContainer>
-        <StyledHeroFooter>
-          <img src="/images/oayc_sign.svg" alt="oayc sign" />
-        </StyledHeroFooter>
-        <StyledApe
-          src="/images/oayc_bg.png"
-          width="384px"
-          height="520px"
-        ></StyledApe>
-      </Container>
-    </>
-  );
+    useEagerConnect();
+    useChangeNetwork();
+    const {connect} = useDefaultConnect();
+    return (
+        <>
+            <Head>
+                <title>STAKING</title>
+                <link rel="icon" href="/main/favicons/red.svg"/>
+            </Head>
+            <Container>
+                <TopBar
+                    logoUrl="/main/favicons/red.svg"
+                    socials={true}
+                    pageType="staking"
+                />
+                <ContentContainer>
+                    <StyledStakingContainer>
+                        <StyledHead>
+                            <StyledText>Stake</StyledText>
+                            <StyledText2>or lock</StyledText2>
+                        </StyledHead>
+                        <StyledButtons>
+                            <StakingRedButton onClick={() => connect()}>
+                                Connect Wallet
+                            </StakingRedButton>
+                            <StakingWhiteButton>
+                                <StyledIcon>
+                                    <StyledText4>Rules and conditions</StyledText4>
+                                    <Image
+                                        src="/images/svg/read_conditions.svg"
+                                        alt="Rules and Conditions"
+                                        width={41}
+                                        height={41}
+                                    />
+                                </StyledIcon>
+                            </StakingWhiteButton>
+                        </StyledButtons>
+                    </StyledStakingContainer>
+                </ContentContainer>
+                <StyledOAYCText>
+                    <Image
+                        src="/images/oayc_sign.svg"
+                        alt="oayc sign"
+                        layout="fill"
+                    />
+                </StyledOAYCText>
+                <StyledApe>
+                    <Image
+                        src="/images/oayc_bg.png"
+                        width={384}
+                        height={520}
+                        alt={""}
+                    />
+                </StyledApe>
+            </Container>
+        </>
+    );
 };
 
 export default Staking;
